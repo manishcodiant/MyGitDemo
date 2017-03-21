e9b6e24d3066349b790b82b26cccf1d42b0c3935
user <manishcodiant@gmail.com>
2017-03-21 15:59:43 +0530

    init

--- app/src/main/java/gs/com/mygitdemo/MainActivity.java
+++ app/src/main/java/gs/com/mygitdemo/MainActivity.java
@@ -8,9 +8,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("Demo","This is my git Demo!!!");
+        Log.e("Demo", "This is my git Demo!!!");
+
+        Log.w("Demo", "This is my dgknd glkdhjdfkjh dfjhl Demo!!!");
     }
 }
